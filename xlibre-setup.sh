43b84e533132d98c5e4fbd1d1cd6aa2cf3eb723d
beamyyl <128243705+beamyyl@users.noreply.github.com>
2026-07-24 12:37:38 +0300

    Create xlibre-setup.sh

---
+++ xlibre-setup.sh
@@ -0,0 +1,11 @@
+sudo pacman -S --needed curl --noconfirm
+curl -O https://xlibre-arch.github.io/xlibre-archlinux.asc
+sudo pacman-key --add xlibre-archlinux.asc
+sudo pacman-key --lsign-key B97F7C613F359424
+rm xlibre-archlinux.asc
+sudo tee -a /etc/pacman.conf > /dev/null << 'EOF'
+[xlibre]
+Server = https://packages.xlibre.net/arch/stable/$arch
+EOF
+sudo pacman -Sy
+sudo pacman -S xlibre-meta
