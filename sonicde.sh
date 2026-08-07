714ecda212a37afcbaf7b8d584db2ad5edb3abca
beamyyl <128243705+beamyyl@users.noreply.github.com>
2026-08-07 11:00:56 +0300

    Delete sonicde.sh

--- sonicde.sh
+++
@@ -1,9 +0,0 @@
-curl -O https://sonicde-arch.github.io/sonicde-archlinux.asc
-sudo pacman-key --add sonicde-archlinux.asc
-sudo pacman-key --finger 3B87898C73F11DF5
-sudo pacman-key --lsign-key 3B87898C73F11DF5
-echo '[sonicde]
-Server = https://sonicde-arch.github.io/$arch' | sudo tee -a /etc/pacman.conf
-sudo pacman -Syyu
-sudo pacman -S sonicde-meta
-sudo systemctl enable soniclogin
